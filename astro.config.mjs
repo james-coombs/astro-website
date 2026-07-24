1bc3201ec8e707cd5511a9612e9180aad91ada9b
james-coombs <coombsj36@gmail.com>
2026-07-24 12:33:18 -0400

    Update deploy.yml, site URL
    - Use node-version: 22
    - Use https

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://james-coombs.com/',
+  site: 'https://james-coombs.com/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
