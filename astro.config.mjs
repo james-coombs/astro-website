c3c7c9641357ecfe31299b956a6b586aa94cedcd
james-coombs <coombsj36@gmail.com>
2026-07-24 12:06:55 -0400

    Make deployment updates
    - Add workflow, url data, configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'http://james-coombs.com/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
