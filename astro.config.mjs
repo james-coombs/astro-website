bc24c31911f627c5e057d87b4d16523aa34fc6ce
james-coombs <coombsj36@gmail.com>
2026-08-08 07:49:35 -0400

    Add trailingSlash in config
    - Always use trailingSlash to prevent GH pages redirects
    - Update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://james-coombs.com/",
+  trailingSlash: "always",
   build: {
     inlineStylesheets: "always",
   },
